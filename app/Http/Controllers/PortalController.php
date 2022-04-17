b21366e66f94cefe3e04fe669071196a0a385ef1
shanejr7 <shane.jr7@icloud.com>
2022-04-17 11:17:22 -0500

    random user storage

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -82,7 +82,7 @@ class PortalController extends Controller
        public function clientAccount()
        {
 
-         if (auth()->guest()) {
+           if (auth()->guest()) {
             abort(403);
             
         }
@@ -103,23 +103,23 @@ class PortalController extends Controller
        public function updateOrderLocation(Request $request)
        {
 
-           $attributes =  $request->validate([
+         $attributes =  $request->validate([
             'shipping_address' => 'required',
             'order_id' => 'required|numeric'
         ]);
 
-           $order_id = $request->input('order_id');
+         $order_id = $request->input('order_id');
 
-           $order_shipping_address = $request->input('shipping_address');
+         $order_shipping_address = $request->input('shipping_address');
 
-           $order = Order::where('id', '=', $order_id)->find($order_id);
+         $order = Order::where('id', '=', $order_id)->find($order_id);
 
-           $order->shipping_address = $order_shipping_address;
+         $order->shipping_address = $order_shipping_address;
 
-           $order->save();
+         $order->save();
 
-           return redirect()->back()->with(['quotes' => Quote::all()])->with(['orders' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','!=','Completed')->get()])->with(['orders_history' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','like','%Completed%')->get()]);
-       }
+         return redirect()->back()->with(['quotes' => Quote::all()])->with(['orders' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','!=','Completed')->get()])->with(['orders_history' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','like','%Completed%')->get()]);
+     }
 
 
 
@@ -132,22 +132,22 @@ class PortalController extends Controller
        public function updateOrderComplete(Request $request)
        {
 
-           $attributes =  $request->validate([
+         $attributes =  $request->validate([
             'order_id' => 'required|numeric'
         ]);
 
-           $order_id = $request->input('order_id');
+         $order_id = $request->input('order_id');
 
-           $order = Order::where('id', '=', $order_id)->find($order_id);
+         $order = Order::where('id', '=', $order_id)->find($order_id);
 
-           $order->shipping_address = $order->address;
+         $order->shipping_address = $order->address;
 
-           $order->status = 'Completed';
+         $order->status = 'Completed';
 
-           $order->save();
+         $order->save();
 
-           return redirect()->back()->with(['quotes' => Quote::all()])->with(['orders' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','!=','Completed')->get()])->with(['orders_history' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','like','%Completed%')->get()])->with('success', 'Order completed!!!');
-       }
+         return redirect()->back()->with(['quotes' => Quote::all()])->with(['orders' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','!=','Completed')->get()])->with(['orders_history' => User::join('orders','orders.client_id','=','users.id')->where('orders.status','like','%Completed%')->get()])->with('success', 'Order completed!!!');
+     }
 
 
      /**
@@ -214,9 +214,9 @@ class PortalController extends Controller
 
         if(sizeof($quote)!=0){
 
-         return view('transaero-transport-logistics-html-template/html/guest_quote_checkout')->with('private_key', $private_key)->with('quotes',$quote);
+           return view('transaero-transport-logistics-html-template/html/guest_quote_checkout')->with('private_key', $private_key)->with('quotes',$quote);
 
-     }else{
+       }else{
 
 
         return view('transaero-transport-logistics-html-template/html/index');
@@ -326,7 +326,6 @@ class PortalController extends Controller
           if (isset($quote->client_id)) {
 
               $order->client_id = $quote->client_id;
-              
           }
 
           $order->category_id = $quote->category_id;
@@ -371,13 +370,13 @@ class PortalController extends Controller
 
         }else{
 
-           return redirect()->back()->withErrors();
+         return redirect()->back()->withErrors();
 
-       }
+     }
 
 
 
-   }
+ }
 
 
 
@@ -532,9 +531,9 @@ class PortalController extends Controller
     public function clientQuoteSubmission(Request $request)
     {
 
-       $request['email']= strtolower($request->input('email'));
+     $request['email']= strtolower($request->input('email'));
 
-       $attributes =  $request->validate([
+     $attributes =  $request->validate([
         'client_id'=>'numeric',
         'name' => 'required|min:3|max:25',
         'phone' => 'required|digits:10',
@@ -545,32 +544,50 @@ class PortalController extends Controller
         'message' =>'required|max:255',
     ]);
 
+       // save random user info if no client id
+     if(!isset($request->input('client_id'))){
+
+        $user = User::where('email','=',$quote->email)->get();
+
+        if (!isset($user)) {
+
+          $user = new User();
+
+          $user->is_admin = false;
+          $user->name = $quote->name;
+          $user->phone = $quote->phone;
+          $user->email = $quote->email;
+          $user->password = $this->randomString();
+          $user = User::create($user);
+
+      }
+
+  }
+
+  if ($attributes) {
 
 
-       if ($attributes) {
+    $quote = new Quote();
+
+    $quote->active = 1;
+    $quote->client_id = $request->input('client_id');
+    $quote->name = $request->input('name');
+    $quote->phone = $request->input('phone');
+    $quote->email = $request->input('email');
+    $quote->location = $request->input('location');
+    $quote->destination = $request->input('destination');
+    $quote->category_id = $request->input('category');
+    $quote->message = $request->input('message');
 
 
-        $quote = new Quote();
-
-        $quote->active = 1;
-        $quote->client_id = $request->input('client_id');
-        $quote->name = $request->input('name');
-        $quote->phone = $request->input('phone');
-        $quote->email = $request->input('email');
-        $quote->location = $request->input('location');
-        $quote->destination = $request->input('destination');
-        $quote->category_id = $request->input('category');
-        $quote->message = $request->input('message');
+    $quote->private_key = $this->randomString();
 
 
-        $quote->private_key = $this->randomString();
+    $quote->save();
 
+    $request->session()->regenerate();
 
-        $quote->save();
-
-        $request->session()->regenerate();
-
-        return redirect()->back()->with('success', 'your quote was sent!'); 
+    return redirect()->back()->with('success', 'your quote was sent!'); 
 
 
         //        # Instantiate the client.
@@ -587,18 +604,18 @@ class PortalController extends Controller
         //     $mgClient->messages()->send($domain, $params);
 
 
-    }else{
+}else{
 
-        return redirect()->to('/#formArea')->withErrors(['name' => 'required|min:3|max:25',
-            'phone' => 'required|digits:10',
-            'email' => 'required|email|max:255',
-            'location' => 'required|max:255',
-            'destination' => 'required|max:255',
-            'category' =>'required',
-            'message' =>'required|max:255',
-        ]);
+    return redirect()->to('/#formArea')->withErrors(['name' => 'required|min:3|max:25',
+        'phone' => 'required|digits:10',
+        'email' => 'required|email|max:255',
+        'location' => 'required|max:255',
+        'destination' => 'required|max:255',
+        'category' =>'required',
+        'message' =>'required|max:255',
+    ]);
 
-    }
+}
 
 
 
@@ -610,45 +627,45 @@ class PortalController extends Controller
      * @return \Illuminate\View\View
      */
 
-          public function approveTaxRequest(Request $request)
-          {
+    //       public function approveTaxRequest(Request $request)
+    //       {
 
-            if (auth()->guest()) {
-                abort(403);
+    //         if (auth()->guest()) {
+    //             abort(403);
 
-            }
+    //         }
 
-            $user_id = Auth::id();
-            $attribute = $user_id;
+    //         $user_id = Auth::id();
+    //         $attribute = $user_id;
 
 
-            if ($attribute) {
+    //         if ($attribute) {
 
-                $taxpayer = Taxpayer::where('user_id','=',$user_id)->first();
-                $taxpayer->tax_submission_status = "Approved";
+    //             $taxpayer = Taxpayer::where('user_id','=',$user_id)->first();
+    //             $taxpayer->tax_submission_status = "Approved";
 
-                $taxpayer->save();
+    //             $taxpayer->save();
 
-                  # Instantiate the client.
-            $mgClient = Mailgun::create('fd00846c1f2fff1319fd0a375c8825e9-cac494aa-53c6b775'); // For US servers
-            $domain = "mg.emgbusinessconsulting.com";
-            $params = array(
-                'from'    => 'egreen@emgbusinessconsulting.com',
-                'to'      => 'egreen@emgbusinessconsulting.com',
-                'subject' => 'Hello',
-                'text'    => 'a tax application was approved by a client for refund & filing!'
-            );
+    //               # Instantiate the client.
+    //         $mgClient = Mailgun::create('fd00846c1f2fff1319fd0a375c8825e9-cac494aa-53c6b775'); // For US servers
+    //         $domain = "mg.emgbusinessconsulting.com";
+    //         $params = array(
+    //             'from'    => 'egreen@emgbusinessconsulting.com',
+    //             'to'      => 'egreen@emgbusinessconsulting.com',
+    //             'subject' => 'Hello',
+    //             'text'    => 'a tax application was approved by a client for refund & filing!'
+    //         );
 
-             # Make the call to the client.
-            $mgClient->messages()->send($domain, $params);
+    //          # Make the call to the client.
+    //         $mgClient->messages()->send($domain, $params);
 
-            return redirect()->route('TaxPortal');
+    //         return redirect()->route('TaxPortal');
 
-        }
+    //     }
 
-        return redirect()->back()->withErrors();
+    //     return redirect()->back()->withErrors();
 
-    } 
+    // } 
 
 
 
@@ -659,11 +676,11 @@ class PortalController extends Controller
      */
        public function randomString(){
 
-         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-         $charactersLength = strlen($characters);
-         $private_key = '';
+           $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+           $charactersLength = strlen($characters);
+           $private_key = '';
 
-         for ($i = 0; $i < 20; $i++) {
+           for ($i = 0; $i < 20; $i++) {
             $private_key .= $characters[rand(0, $charactersLength - 1)];
         }
 
